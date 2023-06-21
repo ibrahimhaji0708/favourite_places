c14632487168d93f8eff6f97d01d8f4392fd96ae
ibrahimhaji0708 <98814268+ibrahimhaji0708@users.noreply.github.com>
2023-06-21 15:00:34 +0530

    Initial commit

---
+++ lib/screens/places_detail.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'package:favourite_places/models/place.dart';
+
+class PlaceDetailScreen extends StatelessWidget {
+  const PlaceDetailScreen({super.key, required this.place});
+
+  final Place place;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(place.title),
+      ),
+      body: Stack(
+        children: [
+          Image.file(
+            place.image,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                const CircleAvatar(),
+                Container(
+                  child: Text(
+                    place.location.address,
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
